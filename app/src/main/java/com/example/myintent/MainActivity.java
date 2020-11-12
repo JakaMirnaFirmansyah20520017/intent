93eafc05c56d9e242d71f9b8bc545d2a51588898
JakaMirnaFirmansyah20520017 <jaka_20520017@stimata.ac.id>
2020-11-12 11:41:47 +0700

    TugasIntent121120
    Penambahan Button Dial dan Pindah Result

--- app/src/main/java/com/example/myintent/MainActivity.java
+++ app/src/main/java/com/example/myintent/MainActivity.java
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import static com.example.myintent.R.id.btn_move_with_data;
+import static com.example.myintent.R.id.start;
+import static com.example.myintent.R.id.tv_result;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    private int REQUEST_CODE = 100;
+    private TextView tvResult;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button btnMoveWithData = findViewById(R.id.btn_move_with_data);
         btnMoveWithData.setOnClickListener(this);
+
+        Button btnDialNumber = findViewById(R.id.btn_dial_number);
+        btnDialNumber.setOnClickListener(this);
+
+        Button btnMoveForResult = findViewById(R.id.btn_move_with_result);
+        btnMoveForResult.setOnClickListener(this);
+
+        tvResult = (TextView)findViewById(R.id.tv_result);
     }
     @SuppressLint("NonConstantResourceId")
     @Override
@@ -37,6 +50,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20);
                 startActivity(moveWithDataIntent);
                 break;
+            case R.id.btn_dial_number:
+                String phoneNumber = "08563619740";
+                Intent dialPhoneNumber = new Intent(Intent.ACTION_DIAL, Uri.parse("tel : " + phoneNumber));
+                startActivity(dialPhoneNumber);
+                break;
+            case R.id.btn_move_with_result:
+                Intent moveForResultIntent = new Intent(MainActivity.this, MoveForResultAcitvity.class);
+                startActivityForResult(moveForResultIntent, REQUEST_CODE);
+                break;
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == MoveForResultAcitvity.RESULT_CODE) {
+                int selectedValue = data.getIntExtra(MoveForResultAcitvity.EXTRA_SELECTED_VALUE, 0);
+                tvResult.setText("Hasil: " + selectedValue);
+            }
         }
     }
 }
